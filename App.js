72577f473fa5bd532fcb09da59260f88d1b50df7
Asmatchd <Asmatchd@gmail.com>
2020-10-16 18:58:03 +0500

    text done

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 
 const App = () => {
   return (
     <View
       style={{
-        height: '100%',
-        width: '100%',
+        flex: 1,
         backgroundColor: '#faf',
       }}>
+      {/* Top View */}
       <View
         style={{
           height: '30%',
@@ -24,6 +24,36 @@ const App = () => {
             width: '50%',
           }}
         />
+      </View>
+
+      {/* Center View */}
+      <View
+        style={{
+          height: '30%',
+          width: '100%',
+          backgroundColor: '#aaf',
+          alignItems: 'flex-end',
+          justifyContent: 'flex-end',
+        }}>
+        <View
+          style={{
+            height: '30%',
+            width: '50%',
+            backgroundColor: '#45a',
+            // alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              color: '#fff',
+              fontSize: 18,
+              fontWeight: 'bold',
+              fontStyle: 'italic',
+              textAlign: 'center', // if text don't align via alignItems: 'center' then we use textAlign: 'center'
+            }}>
+            Study
+          </Text>
+        </View>
       </View>
     </View>
   );
