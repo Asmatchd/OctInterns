a1bb397c8b224fa318bfeea43eb5f4b52c493ca4
Asmatchd <Asmatchd@gmail.com>
2020-10-19 18:55:04 +0500

    flex view done

--- App.js
+++ App.js
@@ -55,6 +55,99 @@ const App = () => {
           </Text>
         </View>
       </View>
+
+      {/* Bottom View */}
+      <View
+        style={{
+          height: '40%',
+          backgroundColor: 'green',
+          // flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            height: '20%',
+            width: '100%',
+            // backgroundColor: '#faf',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View
+            style={{
+              backgroundColor: '#871',
+              height: '100%',
+              width: '30%',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginRight: 50,
+            }}>
+            <Text
+              style={{
+                color: '#fff',
+                fontSize: 22,
+              }}>
+              Left
+            </Text>
+          </View>
+
+          <View
+            style={{
+              backgroundColor: '#6aa',
+              height: '100%',
+              width: '30%',
+              alignItems: 'center',
+              justifyContent: 'center',
+              // marginLeft: 50,
+            }}>
+            <Text
+              style={{
+                color: '#fff',
+                fontSize: 22,
+              }}>
+              Right
+            </Text>
+          </View>
+        </View>
+
+        <View
+          style={{
+            height: '20%',
+            width: '30%',
+            backgroundColor: '#000',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 20,
+            // marginBottom: 20,
+          }}>
+          <Text
+            style={{
+              color: '#fff',
+              fontSize: 20,
+            }}>
+            Bottom 1
+          </Text>
+        </View>
+
+        <View
+          style={{
+            height: '20%',
+            width: '30%',
+            backgroundColor: '#000',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 20,
+          }}>
+          <Text
+            style={{
+              color: '#fff',
+              fontSize: 20,
+            }}>
+            Bottom 2
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
