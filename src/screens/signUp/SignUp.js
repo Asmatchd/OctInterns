b657b3ac2e09110f5e2baba08984f9969afb269e
Asmatchd <Asmatchd@gmail.com>
2020-11-02 19:05:21 +0500

    keyboard aware, responsive screen done

--- src/screens/signUp/SignUp.js
+++ src/screens/signUp/SignUp.js
@@ -2,6 +2,12 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, TextInput} from 'react-native';
 
+import {
+  widthPercentageToDP as w,
+  heightPercentageToDP as h,
+} from 'react-native-responsive-screen';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+
 export class SignUp extends React.Component {
   state = {
     name: 'Study',
@@ -24,97 +30,124 @@ export class SignUp extends React.Component {
 
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'silver',
-        }}>
-        <TextInput
-          onChangeText={(name) => this.setState({name})}
-          style={{
-            height: '10%',
-            width: '100%',
-            backgroundColor: '#fff',
-            color: 'red',
-            // textAlign: 'center',
-            borderWidth: 2,
-            borderRadius: 10,
-          }}
-          placeholder={'Name'}
-          //   placeholderTextColor={'blue'}
-          value={this.state.name}
-          editable={false}
-        />
-        <TextInput
-          onChangeText={(fName) => this.setState({fName})}
-          style={{
-            height: '10%',
-            width: '100%',
-            backgroundColor: '#fff',
-            color: 'red',
-            // textAlign: 'center',
-            borderWidth: 2,
-            borderRadius: 10,
-          }}
-          placeholder={'Father Name'}
-          //   placeholderTextColor={'blue'}
-        />
-        <TextInput
-          onChangeText={(address) => this.setState({address})}
-          style={{
-            height: '10%',
-            width: '100%',
-            backgroundColor: '#fff',
-            color: 'red',
-            // textAlign: 'center',
-            borderWidth: 2,
-            borderRadius: 10,
-          }}
-          placeholder={'Address'}
-          //   placeholderTextColor={'blue'}
-          keyboardType={'email-address'}
-        />
-        <TextInput
-          onChangeText={(phone) => this.setState({phone})}
-          style={{
-            height: '10%',
-            width: '100%',
-            backgroundColor: '#fff',
-            color: 'red',
-            // textAlign: 'center',
-            borderWidth: 2,
-            borderRadius: 10,
-          }}
-          placeholder={'Phone'}
-          //   placeholderTextColor={'blue'}
-          keyboardType={'phone-pad'}
-        />
-
-        {/* Button */}
-
+      <KeyboardAwareScrollView contentContainerStyle={{flexGrow: 2}}>
         <View
           style={{
-            height: '10%',
-            width: '100%',
+            flex: 1,
+            backgroundColor: 'rgba(0,0,0,0.05)',
             alignItems: 'center',
-            marginTop: '4%',
+            // justifyContent: 'center',
           }}>
-          <TouchableOpacity
-            onPress={() => {
-              this.userData();
-            }}
+          <View
             style={{
-              height: '50%',
-              width: '30%',
+              height: h('60%'),
+              width: '90%',
+              backgroundColor: '#fff',
               alignItems: 'center',
-              marginTop: '4%',
-              backgroundColor: '#faf',
               justifyContent: 'center',
+              marginTop: h('14%'),
+              borderRadius: h('2%'),
+              borderColor: 'red',
+              borderWidth: h('0.1%'),
             }}>
-            <Text>Sign Up</Text>
-          </TouchableOpacity>
+            <TextInput
+              onChangeText={(name) => this.setState({name})}
+              style={{
+                height: h('6%'),
+                width: '80%',
+                backgroundColor: '#fff',
+                color: 'red',
+                // textAlign: 'center',
+                borderWidth: h('0.1%'),
+                borderRadius: h('1%'),
+                marginBottom: h('1%'),
+                fontSize: h('2%'),
+              }}
+              placeholder={'Name'}
+              //   placeholderTextColor={'blue'}
+              value={this.state.name}
+              editable={false}
+            />
+            <TextInput
+              onChangeText={(fName) => this.setState({fName})}
+              style={{
+                height: h('6%'),
+                width: '80%',
+                backgroundColor: '#fff',
+                color: 'red',
+                // textAlign: 'center',
+                borderWidth: h('0.1%'),
+                borderRadius: h('1%'),
+                marginBottom: h('1%'),
+              }}
+              placeholder={'Father Name'}
+              //   placeholderTextColor={'blue'}
+            />
+            <TextInput
+              onChangeText={(address) => this.setState({address})}
+              style={{
+                height: h('6%'),
+                width: '80%',
+                backgroundColor: '#fff',
+                color: 'red',
+                // textAlign: 'center',
+                borderWidth: h('0.1%'),
+                borderRadius: h('1%'),
+                marginBottom: h('1%'),
+              }}
+              placeholder={'Address'}
+              //   placeholderTextColor={'blue'}
+              keyboardType={'email-address'}
+            />
+            <TextInput
+              onChangeText={(phone) => this.setState({phone})}
+              style={{
+                height: h('6%'),
+                width: '80%',
+                backgroundColor: '#fff',
+                color: 'red',
+                // textAlign: 'center',
+                borderWidth: h('0.1%'),
+                borderRadius: h('1%'),
+                marginBottom: h('1%'),
+              }}
+              placeholder={'Phone'}
+              //   placeholderTextColor={'blue'}
+              keyboardType={'phone-pad'}
+            />
+
+            {/* Button */}
+
+            <View
+              style={{
+                width: '100%',
+                alignItems: 'center',
+                marginTop: h('2%'),
+              }}>
+              <TouchableOpacity
+                onPress={() => {
+                  this.userData();
+                }}
+                style={{
+                  height: h('5%'),
+                  width: '30%',
+                  alignItems: 'center',
+                  backgroundColor: 'blue',
+                  justifyContent: 'center',
+                  borderRadius: h('1%'),
+                }}>
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontSize: h('3%'),
+                  }}>
+                  Sign Up
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-      </View>
+      </KeyboardAwareScrollView>
     );
   }
 }
