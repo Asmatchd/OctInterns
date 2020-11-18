c135bf9a5688f30ccaeeaebf7060464a62b3f8cb
Asmatchd <Asmatchd@gmail.com>
2020-11-18 18:43:42 +0500

    learing flatlist

--- src/screens/list/List.js
+++ src/screens/list/List.js
@@ -1,6 +1,12 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {View, Text, SafeAreaView, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 
 import {
   widthPercentageToDP as w,
@@ -15,28 +21,89 @@ export class List extends Component {
     data: [
       {
         name: 'Arslan',
+        age: '20',
+        clr: 'blue',
       },
       {
         name: 'Nouman',
+        age: '22',
+        clr: 'green',
       },
       {
         name: 'Test class',
+        age: '24',
+        clr: 'black',
       },
       {
         name: 'Arslan 1',
+        age: '26',
+        clr: 'pink',
       },
       {
         name: 'Nouman 2',
+        age: '27',
+        clr: 'red',
       },
       {
         name: 'Test class 3',
+        age: '28',
+        clr: 'orange',
       },
     ],
+    selectedItem: '',
   };
 
-  add = () => {};
+  design = (item) => (
+    <TouchableOpacity
+      onPress={() => {
+        this.setState({selectedItem: item});
+      }}
+      style={{
+        height: h('10%'),
+        // backgroundColor: '#0008',
+        // backgroundColor: item.clr === undefined ? '#0008' : item.clr,
+        backgroundColor:
+          item.name === this.state.selectedItem.name ? 'purple' : item.clr,
+        alignItems: 'center',
+        justifyContent: 'center',
+        // marginTop: h('1%'),
+        flexDirection: 'row',
+      }}>
+      <View
+        style={{
+          width: '50%',
+        }}>
+        <Text
+          style={{
+            color:
+              item.name === this.state.selectedItem.name ? 'black' : '#fff',
+            fontSize: h('3%'),
+          }}>
+          Name:{'   '}
+          {item.name}
+        </Text>
+      </View>
 
-  // design=()=>()
+      <View
+        style={{
+          width: '50%',
+          alignItems: 'center',
+        }}>
+        <Text style={{color: '#fff', fontSize: h('3%')}}>
+          Age:{'   '}
+          {item.age}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+  separator = () => (
+    <View
+      style={{
+        height: h('1%'),
+      }}
+    />
+  );
 
   render() {
     return (
@@ -48,21 +115,9 @@ export class List extends Component {
         <NavHeader title={'Flat List'} />
         <FlatList
           data={this.state.data}
-          renderItem={({item}) => (
-            <View
-              style={{
-                height: h('10%'),
-                backgroundColor: '#0008',
-                alignItems: 'center',
-                justifyContent: 'center',
-                marginTop: h('1%'),
-              }}>
-              <Text style={{color: '#fff', fontSize: h('3%')}}>
-                {item.name}
-              </Text>
-            </View>
-          )}
+          renderItem={({item}) => this.design(item)}
           keyExtractor={(item) => item.name}
+          ItemSeparatorComponent={() => this.separator()}
         />
       </View>
     );
