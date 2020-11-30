9935d162f85c008170b1f7005c55a5da39c08d3a
Asmatchd <Asmatchd@gmail.com>
2020-11-30 18:58:05 +0500

    modal done

--- src/screens/signUp/SignUp.js
+++ src/screens/signUp/SignUp.js
@@ -1,24 +1,19 @@
 /* eslint-disable no-alert */
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {
   View,
   Text,
   TouchableOpacity,
-  TextInput,
   Image,
   ImageBackground,
+  Modal,
 } from 'react-native';
 
-import {
-  widthPercentageToDP as w,
-  heightPercentageToDP as h,
-} from 'react-native-responsive-screen';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import {AppBtn, AppInput, Bar, NavHeader} from '../../components';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {Loading} from '../../components';
+import {styles} from './styles';
 export class SignUp extends React.Component {
   state = {
     name: 'Study',
@@ -26,6 +21,12 @@ export class SignUp extends React.Component {
     address: '',
     phone: '+92',
     isLoading: false,
+    modalVisible: false,
+
+    // SIgn In states
+
+    userName: '',
+    password: '',
   };
 
   userData = () => {
@@ -58,23 +59,22 @@ export class SignUp extends React.Component {
     }
   };
 
+  signIn = () => {
+    console.warn('User name is = ' + this.state.userName);
+    console.warn('User password is = ' + this.state.password);
+  };
+
   controlLoading = (value) => {
     this.setState({isLoading: value});
   };
 
   render() {
     return (
-      <KeyboardAwareScrollView contentContainerStyle={{flexGrow: 2}}>
+      <KeyboardAwareScrollView contentContainerStyle={styles.container}>
         <ImageBackground
           source={require('../../assets/bg.jpg')}
           resizeMode={'cover'}
-          style={{
-            height: h('96.5%'),
-            width: w('100%'),
-            backgroundColor: 'rgba(0,0,0,0.05)',
-            // alignItems: 'center',
-            // justifyContent: 'center',
-          }}>
+          style={styles.imgBg}>
           <Bar clr={'red'} />
 
           <Loading showLoading={this.state.isLoading} />
@@ -88,44 +88,16 @@ export class SignUp extends React.Component {
             rightIcPressed={() => this.userData()}
           />
 
-          <View
-            style={{
-              alignItems: 'center',
-              width: w('100%'),
-            }}>
-            <View
-              style={{
-                height: h('60%'),
-                width: '90%',
-                backgroundColor: '#fff4',
-                // alignItems: 'center',
-                // justifyContent: 'center',
-                marginTop: h('10%'),
-                borderRadius: h('2%'),
-                borderColor: '#fff9',
-                borderWidth: h('0.1%'),
-              }}>
-              <View
-                style={{
-                  height: h('15%'),
-                  width: '100%',
-                  // backgroundColor: '#faf',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
+          <View style={styles.contentContainer}>
+            <View style={styles.userInfo}>
+              <View style={styles.imgView}>
                 {/* https://images.agoramedia.com/wte3.0/gcms/Sleeping-Through-the-Night-722x406.jpg?width=414 */}
                 <TouchableOpacity
                   onPress={() => {
                     console.warn('Img pressed');
                   }}>
                   <Image
-                    style={{
-                      height: h('13%'),
-                      width: h('13%'),
-                      // resizeMode: 'contain',
-                      borderRadius: h('8%'),
-                      overflow: 'hidden',
-                    }}
+                    style={styles.img}
                     // resizeMode={'contain'}
                     source={require('../../assets/logo.jpg')}
                     // source={{
@@ -142,22 +114,14 @@ export class SignUp extends React.Component {
                 editable={false}
                 placeholder={'Name'}
               />
-              <View
-                style={{
-                  height: h('1.5%'),
-                }}
-              />
+              <View style={styles.spacer} />
 
               <AppInput
                 onChangeText={(fName) => this.setState({fName})}
                 placeholder={'Father Name'}
               />
 
-              <View
-                style={{
-                  height: h('1.5%'),
-                }}
-              />
+              <View style={styles.spacer} />
 
               <AppInput
                 onChangeText={(address) => this.setState({address})}
@@ -167,11 +131,7 @@ export class SignUp extends React.Component {
                 autoCapitalize={'none'}
               />
 
-              <View
-                style={{
-                  height: h('1.5%'),
-                }}
-              />
+              <View style={styles.spacer} />
 
               <AppInput
                 onChangeText={(phone) => this.setState({phone})}
@@ -182,11 +142,7 @@ export class SignUp extends React.Component {
                 maxLength={13}
               />
 
-              <View
-                style={{
-                  height: h('1.5%'),
-                }}
-              />
+              <View style={styles.spacer} />
 
               {/* Button */}
 
@@ -196,9 +152,100 @@ export class SignUp extends React.Component {
                   this.userData();
                 }}
               />
+
+              <View style={styles.btnView}>
+                <TouchableOpacity
+                  onPress={() => {
+                    this.setState({modalVisible: true});
+                  }}
+                  style={styles.btn}>
+                  <Text style={styles.btnTxt}>
+                    Already have an account? Sign In
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         </ImageBackground>
+
+        {/*=======>>>>>>>      Sign In modal       <<<<<<<<<<============ */}
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert('Modal has been closed.');
+          }}>
+          <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+            <ImageBackground
+              source={require('../../assets/signInBg.jpg')}
+              resizeMode={'cover'}
+              style={styles.imgBg}>
+              <Bar clr={'red'} />
+
+              <Loading showLoading={this.state.isLoading} />
+
+              <NavHeader
+                title={'Sign In'}
+                leftIc={'ios-arrow-back'}
+                leftIcPressed={() => {
+                  this.setState({modalVisible: false});
+                }}
+              />
+
+              <View style={styles.contentContainer}>
+                <View style={styles.userInfo}>
+                  <View style={styles.imgView}>
+                    <Image
+                      style={styles.img}
+                      // resizeMode={'contain'}
+                      source={require('../../assets/logo.jpg')}
+                      // source={{
+                      //   uri:
+                      //     'https://images.agoramedia.com/wte3.0/gcms/Sleeping-Through-the-Night-722x406.jpg?width=414',
+                      // }}
+                    />
+                  </View>
+
+                  <AppInput
+                    onChangeText={(userName) => this.setState({userName})}
+                    placeholder={'User Name'}
+                  />
+                  <View style={styles.spacer} />
+
+                  <AppInput
+                    onChangeText={(password) => this.setState({password})}
+                    placeholder={'Password'}
+                    secureTextEntry
+                  />
+
+                  <View style={styles.spacer} />
+
+                  {/* Button */}
+
+                  <AppBtn
+                    txt={'Sign In'}
+                    btnPressed={() => {
+                      this.signIn();
+                    }}
+                  />
+
+                  <View style={styles.btnView}>
+                    <TouchableOpacity
+                      onPress={() => {
+                        this.setState({modalVisible: false});
+                      }}
+                      style={styles.btn}>
+                      <Text style={styles.btnTxt}>
+                        Don't have an account? Sign Up
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            </ImageBackground>
+          </KeyboardAwareScrollView>
+        </Modal>
       </KeyboardAwareScrollView>
     );
   }
